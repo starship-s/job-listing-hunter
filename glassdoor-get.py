13d80f1d8d41ecbe3528a675890f32a6fbdbf0c4
Justin <jmaughan@rocketmail.com>
2019-10-30 00:02:17 -0600

    Glassdoor functionality ready

---
+++ glassdoor-get.py
@@ -0,0 +1,102 @@
+from bs4 import BeautifulSoup
+import datetime
+import requests
+import pymysql
+import datetime
+import hashlib
+import time
+import json
+from pathlib import Path
+
+with open(str(Path(__file__).resolve().parent)+'\credentials.json') as json_file:
+    credentials = json.load(json_file)
+
+titles = ['data scientist', 'data engineer', 'data analyst']
+locations = ['salt lake city']
+page = '1'
+jobs = []
+now = datetime.datetime.now()
+now = now.strftime("%Y-%m-%d %H:%M")
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+query = (
+        "INSERT INTO indeed (`key`, `time`, `Company_Name`, `Job_Title`, `Query`, `Location`) VALUES (%s, %s, %s, %s, %s, %s)")
+
+
+def process_page(location, title, page):
+    url_indexing = len(title) + 15
+    url = 'https://www.glassdoor.com/Job/' + location + '-' + title + '-jobs-SRCH_IL.0,14_IC1128289_KO15,' + \
+          str(url_indexing)+'_IP' + str(page) + '.htm'
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    this_page = []
+
+    results = soup.find_all('div', attrs={'class': 'jobContainer'})
+    last_page = soup.find_all('li', attrs={'class': 'page current last'})
+
+    for x in results:
+        this_job = []
+
+        company = x.find('div', attrs={"class": "jobInfoItem jobEmpolyerName"}).text.strip()
+        job_title = x.find_all('a', attrs={"class": "jobTitle"})[1].text.strip()
+        this_job.append(company)
+        this_job.append(job_title)
+
+        this_page.append(this_job)
+
+    jobs.append(this_page)
+
+    print(url)
+
+    if len(last_page) == 0:
+        return True
+    else:
+        return False
+
+
+for location in locations:
+    for title in titles:
+        while process_page(location.replace(' ', '-'), title.replace(' ', '-'), page) is True:
+            page = str(int(page) + 1)
+
+
+
+        for pages in jobs:
+            for x in pages:
+
+                connection = pymysql.connect(user=credentials['user'],
+                                             password=credentials['password'],
+                                             host=credentials['host'],
+                                             database=credentials['database'])
+
+                print(x)
+                while True:
+                    try:
+                        cursor = connection.cursor()
+                        hashing = x[0] + x[1]
+                        hashing = hashlib.md5(hashing.encode())
+                        hashing = hashing.hexdigest()
+                        print(hashing)
+                        values = hashing, now, x[0], x[1], title, location
+                        cursor.execute(query, values)
+                        cursor.close()
+                        connection.commit()
+                        connection.close()
+                    except pymysql.err.IntegrityError:
+                        time.sleep(30)
+                        print('int error')
+                        continue
+                    except pymysql.err.OperationalError:
+                        time.sleep(30)
+                        print('opp error')
+                        continue
+                    else:
+                        break
+
+        page = '1'
+
+
+print(jobs)
+
+
